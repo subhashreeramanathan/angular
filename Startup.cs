ff70dc2d274ef308971a9d3554f2f8ec7a989476
subhashreeramanathan <subhashree.ramanathan@syncfusion.com>
2017-06-20 17:39:42 +0530

    updated file

--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace seed
+namespace ejangularapplication
 {
     public class Startup
     {
